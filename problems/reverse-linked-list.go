a2641b74fa65d316b8890ef743833f8b991bfd06
yb <136864718@qq.com>
2021-06-23 00:48:51 +0800

    add code

---
+++ problems/reverse-linked-list.go
@@ -0,0 +1,54 @@
+package problems
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// reverseListM1 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+// 暴力解法，先存在数组，再reverse
+func reverseListM1(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	var nodes []*ListNode
+	for head != nil {
+		nodes = append(nodes, head)
+		head = head.Next
+	}
+
+	for i := len(nodes) - 1; i > 0; i-- {
+		nodes[i-1].Next = nil
+		nodes[i].Next = nodes[i-1]
+
+	}
+
+	return nodes[len(nodes)-1]
+}
+
+// 在遍历链表时，将当前节点的 \textit{next}next 指针改为指向前一个节点。
+// 由于节点没有引用其前一个节点，因此必须事先存储其前一个节点。
+// 在更改引用之前，还需要存储后一个节点。最后返回新的头引用。
+func reverseListE1(head *ListNode) *ListNode {
+	var prev *ListNode
+	curr := head
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+	return prev
+}
+
+func reverseListE2(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	newHead := reverseListE2(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return newHead
+
+}
---
+++ problems/swap-nodes-in-pairs.go
@@ -0,0 +1,12 @@
+package problems
+
+// 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+//你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+//
+// 还是使用双指针，但是需要区分偶节点
+// https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+func swapPairsM1(head *ListNode) *ListNode {
+	var currNode *ListNode
+
+	return nil
+}
