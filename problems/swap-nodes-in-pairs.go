73be6ef350425c2589ae0eb25608775cc42b4cf5
yangbo <506813017@qq.com>
2021-06-23 21:27:23 +0800

    add code

--- problems/swap-nodes-in-pairs.go
+++ problems/swap-nodes-in-pairs.go
@@ -5,8 +5,46 @@ package problems
 //
 // 还是使用双指针，但是需要区分偶节点
 // https://leetcode-cn.com/problems/swap-nodes-in-pairs/
-func swapPairsM1(head *ListNode) *ListNode {
-	// var currNode *ListNode
+func SwapPairsM1(head *ListNode) *ListNode {
+	curr := 0
+	var preNode, nextNode *ListNode
 
-	return nil
+	var newHead *ListNode
+
+	for head.Next != nil {
+		if curr == 1 {
+			newHead = head
+		}
+		if curr%2 == 0 {
+			// 偶数则保存curr和next
+			preNode = head
+			head = head.Next
+			nextNode = head.Next
+		} else {
+			head.Next = preNode
+			preNode.Next = nextNode
+
+			head = preNode
+		}
+		curr++
+
+	}
+
+	preNode.Next = head
+
+	return newHead
+}
+
+func SwapPairsE1(head *ListNode) *ListNode {
+	dummyHead := &ListNode{0, head}
+	temp := dummyHead
+	for temp.Next != nil && temp.Next.Next != nil {
+		node1 := temp.Next
+		node2 := temp.Next.Next
+		temp.Next = node2
+		node1.Next = node2.Next
+		node2.Next = node1
+		temp = node1
+	}
+	return dummyHead.Next
 }
