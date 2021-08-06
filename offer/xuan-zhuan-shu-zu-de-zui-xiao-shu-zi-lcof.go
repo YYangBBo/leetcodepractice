61571f5f4698bba9692b6d8d85b7d199315423ef
yangbo <yangbo@shihuituan.net>
2021-08-06 21:40:10 +0800

    add code

---
+++ offer/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof.go
@@ -0,0 +1,23 @@
+package offer
+
+// 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。
+//例如，数组[3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。
+
+//链接：https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof
+func minArray(numbers []int) int {
+	left ,right := 0,len(numbers)-1
+
+	for left < right {
+		mid := left + (right - left ) / 2
+
+		if numbers[mid] < numbers[right] {
+			right = mid
+		}else if numbers[mid] > numbers[right]{
+			left = mid + 1
+		}else {
+			right --
+		}
+	}
+
+	return numbers[left]
+}
