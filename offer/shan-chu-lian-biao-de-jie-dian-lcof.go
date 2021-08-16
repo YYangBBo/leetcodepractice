c57909418245c34f3bcc0421b438ca2a6e50eb8e
yb <136864718@qq.com>
2021-08-17 00:07:25 +0800

    add code

---
+++ offer/shan-chu-lian-biao-de-jie-dian-lcof.go
@@ -0,0 +1,23 @@
+package offer
+
+// 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+//返回删除后的链表的头节点。
+//注意：此题对比原题有改动
+// https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof/
+func deleteNode(head *ListNode, val int) *ListNode {
+	if head.Val == val {
+		return head.Next
+	}
+
+	headNode,preNode := head,head
+	for head != nil {
+		if head.Val == val {
+			preNode.Next = head.Next
+			break
+		}
+		preNode = head
+		head = head.Next
+	}
+
+	return headNode
+}
