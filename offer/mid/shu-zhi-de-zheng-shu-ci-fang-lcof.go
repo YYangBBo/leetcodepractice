7ff2cc5334d8f86f74a83565572fad92fb458c44
yb <136864718@qq.com>
2021-08-31 00:17:32 +0800

    add code

---
+++ offer/mid/shu-zhi-de-zheng-shu-ci-fang-lcof.go
@@ -0,0 +1,9 @@
+package mid
+
+// 实现 pow(x, n) ，即计算 x 的 n 次幂函数（即，xn）。不得使用库函数，同时不需要考虑大数问题。
+// https://leetcode-cn.com/problems/shu-zhi-de-zheng-shu-ci-fang-lcof/
+func myPow(x float64, n int) float64 {
+
+
+	return 0
+}
