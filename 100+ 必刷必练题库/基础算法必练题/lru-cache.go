56aa95ed54ec3fe917b0ba627b95ecbe21483693
yb <136864718@qq.com>
2021-09-17 12:40:23 +0800

    add code

---
+++ 100+ 必刷必练题库/基础算法必练题/lru-cache.go
@@ -0,0 +1,29 @@
+package code
+
+
+type LRUCache struct {
+
+}
+
+
+func Constructor(capacity int) LRUCache {
+	return LRUCache{}
+}
+
+
+func (this *LRUCache) Get(key int) int {
+	return 0
+}
+
+
+func (this *LRUCache) Put(key int, value int)  {
+
+}
+
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * obj := Constructor(capacity);
+ * param_1 := obj.Get(key);
+ * obj.Put(key,value);
+ */
